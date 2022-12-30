a157ad37c7d61d8dbbc777639e5d11f8dca644db
Joshua Doud <bigjj3393@yahoo.com>
2022-12-30 11:51:29 -0800

    commit tree 3ac0182d46d0d92715fc3a0e3078f9b7043c72d7

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [search, setSearch] = useState("")
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data))
+  }, [])
+
+  function handleAddPlant (newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  const filteredPlants = plants.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search search={search} onSearch={setSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
